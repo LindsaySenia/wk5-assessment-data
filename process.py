11346fc88a5b0e2b14e897fca997b435a9b83940
LindsaySenia <lmzsenia@gmail.com>
2021-11-13 11:26:38 -0500

    done

---
+++ process.py
@@ -0,0 +1,22 @@
+log_file = open("um-server-01.txt")
+# This is opening the txt file within the python file so the code we write can access the data storted here, like it would with a csv file. It also sets the txt file equal to a variable (log_file) so it can be called upon or passed into functions
+
+
+def sales_reports(log_file):
+# This is creating a function called sales_report, where a variable is passed in (log_file is just a placeholder here, but indicates we will likely be passing in that txt file).
+    for line in log_file:
+    # each line in the file is parsed seperately
+        line = line.rstrip()
+        # The whitespace and linebreak is removed from the end of each line
+        day = line[0:3]
+        # This creates a variable 'day' which is equal to 
+        if day == "Mon":
+        # If the day is equal to 'Tue'
+            print(line)
+            # Then the line is printed (like console.log in JS)
+
+
+
+
+sales_reports(log_file)
+# Invoking the sales_reports function passing in the log_file
